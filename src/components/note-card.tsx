609efedbff91c6675b95d92ead3ca01e2f0a124a
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2024-02-06 22:36:45 -0300

    feat: Acessar dialogos para criação de notas em texto

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,17 +1,52 @@
-export function NoteCard() {
+import * as Dialog from "@radix-ui/react-dialog";
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import { X } from "lucide-react";
+
+interface NoteCardProps {
+  note: {
+    date: Date;
+    content: string;
+  };
+}
+
+export function NoteCard({ note }: NoteCardProps) {
   return (
-    <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
-      <span className="text-sm font-medium text-slate-300">Há 2 dias</span>
-      <p className="text-sm leading-6 text-slate-400">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem
-        quisquam eius aperiam amet tempore, unde quaerat et laboriosam pariatur.
-        Doloremque, sunt! Praesentium quam cupiditate cum aliquid repellendus.
-        Laborum, aspernatur ab. Lorem ipsum dolor, sit amet consectetur
-        adipisicing elit. Exercitationem quisquam eius aperiam amet tempore,
-        unde quaerat et laboriosam pariatur. Doloremque, sunt! Praesentium quam
-        cupiditate cum aliquid repellendus. Laborum, aspernatur ab.
-      </p>
-      <div className="absolute bottom-0 right-0 left-0 h-1/2  bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-    </button>
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+        <span className="text-sm font-medium text-slate-300">
+          {note.date.toISOString()}
+        </span>
+        <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+        <div className="absolute bottom-0 right-0 left-0 h-1/2  bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 bg-black/50" />
+        <Dialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none overflow-hidden">
+          <Dialog.Close className="absolute right-0 top-0 p-1.5 bg-slate-800 text-slate-400 hover:text-slate-100">
+            <X className="size-5" />
+          </Dialog.Close>
+          <div className="flex flex-1 flex-col gap-3 p-5">
+            <span className="text-sm font-medium text-slate-300">
+              {formatDistanceToNow(note.date, {
+                addSuffix: true,
+                locale: ptBR,
+              })}
+            </span>
+            <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+          </div>
+          <button
+            type="button"
+            className="w-full py-4 bg-slate-800 text-center text-sm text-slate-300 outline-none font-medium group"
+          >
+            Deseja{" "}
+            <span className="text-red-400 group-hover:underline">
+              apagar essa nota
+            </span>
+            ?
+          </button>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
